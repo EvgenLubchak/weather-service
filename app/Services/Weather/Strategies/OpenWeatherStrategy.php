5866cdfe618daec7c60aa9a5acbf3dc002d7e8c9
evgen <evgencs4@gmail.com>
2025-06-23 14:43:32 +0300

    try catch on get temperature api call

--- app/Services/Weather/Strategies/OpenWeatherStrategy.php
+++ app/Services/Weather/Strategies/OpenWeatherStrategy.php
@@ -6,28 +6,33 @@ use App\ValueObjects\Location;
 use App\Services\Weather\TemperatureProviderInterface;
 use App\ValueObjects\TemperatureCelsius;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\ConnectionException;
 
 class OpenWeatherStrategy implements TemperatureProviderInterface
 {
     /**
      * @param Location $location
      * @return TemperatureCelsius
-     * @throws \Illuminate\Http\Client\ConnectionException
+     * @throws \Exception When API request fails or connection issues occur
      */
     public function getCurrentTemperatureCelsius(Location $location): TemperatureCelsius
     {
-        $response = Http::get(config('weather.providers.openweather.api_url'), [
-            'lat' => $location->latitude,
-            'lon' => $location->longitude,
-            'exclude' => 'minutely,hourly,daily,alerts',
-            'appid' => config('weather.providers.openweather.api_key'),
-            'units' => 'metric',
-        ]);
+        try {
+            $response = Http::get(config('weather.providers.openweather.api_url'), [
+                'lat' => $location->latitude,
+                'lon' => $location->longitude,
+                'exclude' => 'minutely,hourly,daily,alerts',
+                'appid' => config('weather.providers.openweather.api_key'),
+                'units' => 'metric',
+            ]);
 
-        if ($response->successful()) {
-            return new TemperatureCelsius($response->json('current.temp'));
+            if ($response->successful()) {
+                return new TemperatureCelsius($response->json('current.temp'));
+            }
+
+            throw new \Exception("OpenWeather API error: " . $response->body());
+        } catch (ConnectionException $e) {
+            throw new \Exception("OpenWeather API connection error: " . $e->getMessage());
         }
-
-        throw new \Exception("OpenWeather API error: " . $response->body());
     }
 }
